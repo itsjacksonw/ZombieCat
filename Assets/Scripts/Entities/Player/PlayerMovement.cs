caa6e22ee0f487426575e8f65edf83d580ab8a34
Jackson Westbrook <infamousjackson19@gmail.com>
2022-10-01 20:59:17 -0400

    Update PlayerMovement.cs

--- Assets/Scripts/Entities/Player/PlayerMovement.cs
+++ Assets/Scripts/Entities/Player/PlayerMovement.cs
@@ -91,7 +91,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Enemy")
         {
             isGrounded = true;
             isLaunching = false;
