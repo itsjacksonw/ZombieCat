74fede89482e99abe5eb023f84c9c955c3381cef
Jackson Westbrook <infamousjackson19@gmail.com>
2022-10-02 09:41:54 -0400

    bc

---
+++ Assets/Scripts/Utilities/NextScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "NextScene") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
