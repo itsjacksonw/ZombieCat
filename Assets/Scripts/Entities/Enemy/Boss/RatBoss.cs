c1b88b4d15b82782671f726bbaf46567b05610c3
Billy McCarthy <wmccarthy2401@gmail.com>
2022-10-01 20:36:12 -0400

    THE RAT (origin)

---
+++ Assets/Scripts/Entities/Enemy/Boss/RatBoss.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatBoss : EntityBase
+{
+    //Basic rendering and animation
+    Animator anim;
+    SpriteRenderer rend;
+
+    Rigidbody2D rb;
+
+    [SerializeField]
+    private Transform target;
+    private int dir = 1;
+
+    [SerializeField]
+    private float turnaroundRate = 3;
+    private float turnaroundTime = 0;
+
+    [SerializeField]
+    private Transform meleePoint;
+    [SerializeField]
+    private LayerMask playerLayer;
+    [SerializeField]
+    private float attackRange = 0.41F;
+    [SerializeField]
+    private float detectRange = 0.6F;
+    [SerializeField]
+    private float meleeRate = 2;
+    [SerializeField]
+    private float nextAttackTime = 0;
+
+    private bool isAttacking = false;
+
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rend = this.GetComponent<SpriteRenderer>();
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        #region UpdateMeleePoint
+        float xLoc = Mathf.Abs(meleePoint.localPosition.x);
+        if (rend.flipX)
+        {
+            Debug.Log("Melee Left");
+            meleePoint.SetLocalPositionAndRotation(new Vector3(-xLoc, meleePoint.localPosition.y, 0), meleePoint.localRotation);
+        }
+        else
+        {
+            meleePoint.SetLocalPositionAndRotation(new Vector3(xLoc, meleePoint.localPosition.y, 0), meleePoint.localRotation);
+        }
+        #endregion
+
+        if (!isAttacking)
+        {
+            Move();
+        }
+        if (Physics2D.OverlapCircleAll(meleePoint.position, detectRange, playerLayer).Length > 0 && !isAttacking)
+        {
+            rb.velocity = Vector2.zero;
+            anim.SetFloat("Speed", 0);
+            isAttacking = true;
+            nextAttackTime = Time.time + 1 / meleeRate;
+        }
+        if (isAttacking && Time.time >= nextAttackTime)
+        {
+            Melee();
+            isAttacking = false;
+        }
+
+    }
+
+    public override void Move()
+    {
+        if (Time.time >= turnaroundTime)
+        {
+            if (target.position.x > this.transform.position.x && dir != 1)
+            {
+                dir = 1;
+                turnaroundTime = Time.time + 1f / turnaroundRate;
+            }
+            else if (target.position.x < this.transform.position.x && dir != -1)
+            {
+                dir = -1;
+                turnaroundTime = Time.time + 1f / turnaroundRate;
+            }
+        }
+
+
+        //Left or right?
+        if (dir > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (dir < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(dir));
+
+        Vector2 targetVel = new Vector2(dir * speed * 100 * Time.deltaTime, rb.velocity.y);
+        rb.velocity = targetVel;
+
+    }
+
+    private void Melee()
+    {
+
+        //Play attack animation
+        anim.SetTrigger("Attacking");
+
+        //Detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(meleePoint.position, attackRange, playerLayer);
+
+
+        //Damage enemies
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EntityBase>().takeDamage(attackPower);
+        }
+    }
+    public override void onDeath()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (meleePoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(meleePoint.position, attackRange);
+        //Gizmos.DrawWireSphere(meleePoint.position, detectRange);
+    }
+
+}
